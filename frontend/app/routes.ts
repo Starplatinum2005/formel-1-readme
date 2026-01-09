4186cc544f72daff9fe796b7ebf9e34a8e616adc
Starplatinum2005 <stefanstanisic2005@gmail.com>
2026-01-09 17:35:57 +0100

    header

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -3,7 +3,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
     index("routes/startseite/startseite.tsx"),
     route("shop", "routes/shop/shop.tsx"),
-    route("admin/add-product", "routes/admin/add-product.tsx")
-    
+    route("admin/add-product", "routes/admin/add-product.tsx"),
+    route("tracks", "routes/Bahnen/bahnen.tsx"),  
     
 ] satisfies RouteConfig;
